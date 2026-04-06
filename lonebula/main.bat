82b6ba6e3080309655e4159eef8abbe761d23de5
shogo-sub <shogo.shogo.lolo@gmail.com>
2026-04-06 14:52:37 +0900

    s

---
+++ lonebula/main.bat
@@ -0,0 +1,18 @@
+@echo off
+set CONDA_BASE=C:\Users\shogo\anaconda3
+set ENV_NAME=ssi310
+set SSI_PATH=C:\ssi\bin\x64\vc140
+
+call %CONDA_BASE%\Scripts\activate.bat %CONDA_BASE%\envs\%ENV_NAME%
+set PATH=%SSI_PATH%;%CONDA_PREFIX%;%CONDA_PREFIX%\Library\bin;%CONDA_PREFIX%\Scripts;%PATH%
+
+set PYTHONHOME=%CONDA_PREFIX%
+set PYTHONPATH=C:\ssi\lonebula;%CONDA_PREFIX%\Lib;%CONDA_PREFIX%\DLLs
+
+echo --------------------------------------------------
+echo [SSI] Starting System
+echo ENV: %CONDA_PREFIX%
+echo --------------------------------------------------
+
+cd /d C:\ssi\lonebula
+C:\ssi\bin\x64\vc140\xmlpipe.exe C:\ssi\lonebula\main.pipeline
